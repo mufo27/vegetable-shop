a176c8ba29c029e7d2cf93ccf4940ca5e2a45003
Mew <>
2022-12-15 12:29:50 +0700

    index customer

--- role_customer/index.php
+++ role_customer/index.php
@@ -57,21 +57,19 @@
                         <div class="col-sm-6 col-xl-3">
                             <div class="p-3 bg-primary-300 rounded overflow-hidden position-relative text-white mb-g">
                                 <div class="">
-                                    <h3 class="display-4 d-block l-h-n m-0 fw-500">
-                                        21.5k
-                                        <small class="m-0 l-h-n">users signed up</small>
-                                    </h3>
+                                    <a href="#" style="color:aliceblue" class="display-4 d-block l-h-n m-0 fw-500">
+                                        ตะกร้าสินค้า
+                                    </a>
                                 </div>
-                                <i class="fal fa-user position-absolute pos-right pos-bottom opacity-15 mb-n1 mr-n1" style="font-size:6rem"></i>
+                                <i class="fal fa-shopping-basket position-absolute pos-right pos-bottom opacity-15 mb-n1 mr-n1" style="font-size:6rem"></i>
                             </div>
                         </div>
                         <div class="col-sm-6 col-xl-3">
                             <div class="p-3 bg-warning-400 rounded overflow-hidden position-relative text-white mb-g">
                                 <div class="">
-                                    <h3 class="display-4 d-block l-h-n m-0 fw-500">
-                                        $10,203
-                                        <small class="m-0 l-h-n">Visual Index Figure</small>
-                                    </h3>
+                                    <a href="#" style="color:aliceblue" class="display-4 d-block l-h-n m-0 fw-500">
+                                        รายการสั่งซื้อสินค้า
+                                    </a>
                                 </div>
                                 <i class="fal fa-gem position-absolute pos-right pos-bottom opacity-15  mb-n1 mr-n4" style="font-size: 6rem;"></i>
                             </div>
@@ -79,10 +77,9 @@
                         <div class="col-sm-6 col-xl-3">
                             <div class="p-3 bg-success-200 rounded overflow-hidden position-relative text-white mb-g">
                                 <div class="">
-                                    <h3 class="display-4 d-block l-h-n m-0 fw-500">
-                                        - 103.72
-                                        <small class="m-0 l-h-n">Offset Balance Ratio</small>
-                                    </h3>
+                                    <a href="#" style="color:aliceblue" class="display-4 d-block l-h-n m-0 fw-500">
+                                        การจัดส่งสินค้า
+                                    </a>
                                 </div>
                                 <i class="fal fa-lightbulb position-absolute pos-right pos-bottom opacity-15 mb-n5 mr-n6" style="font-size: 8rem;"></i>
                             </div>
@@ -90,10 +87,9 @@
                         <div class="col-sm-6 col-xl-3">
                             <div class="p-3 bg-info-200 rounded overflow-hidden position-relative text-white mb-g">
                                 <div class="">
-                                    <h3 class="display-4 d-block l-h-n m-0 fw-500">
-                                        +40%
-                                        <small class="m-0 l-h-n">Product level increase</small>
-                                    </h3>
+                                    <a href="#" style="color:aliceblue" class="display-4 d-block l-h-n m-0 fw-500">
+                                        ประวัติการสั่งซื้อสินค้า
+                                    </a>
                                 </div>
                                 <i class="fal fa-globe position-absolute pos-right pos-bottom opacity-15 mb-n1 mr-n4" style="font-size: 6rem;"></i>
                             </div>
